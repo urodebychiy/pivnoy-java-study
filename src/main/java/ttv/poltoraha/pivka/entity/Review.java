4bf91bb02a119ac82aa5a4864fab70bb02a4adc4
urodebychiy <umbrachka@gmail.com>
2025-05-20 11:23:21 +0300

    установил правило, чтобы не было импортов вида import java.util.*

--- src/main/java/ttv/poltoraha/pivka/entity/Review.java
+++ src/main/java/ttv/poltoraha/pivka/entity/Review.java
@@ -1,6 +1,11 @@
 package ttv.poltoraha.pivka.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
