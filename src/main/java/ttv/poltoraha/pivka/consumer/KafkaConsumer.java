0de8ac9b959047246fb75884f73354bc86556eb1
urodebychiy <umbrachka@gmail.com>
2025-05-23 19:37:01 +0300

    навайбкодил

--- src/main/java/ttv/poltoraha/pivka/consumer/KafkaConsumer.java
+++ src/main/java/ttv/poltoraha/pivka/consumer/KafkaConsumer.java
@@ -1,6 +1,5 @@
 package ttv.poltoraha.pivka.consumer;
 
-import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 
@@ -8,8 +7,8 @@ import org.springframework.stereotype.Service;
 @Service
 public class KafkaConsumer {
 
-    @KafkaListener(topics = "your-topic", groupId = "my-group")
-    public void listen(String message) {
-        System.out.println("Received message: " + message);
-    }
+//    @KafkaListener(topics = "your-topic", groupId = "my-group")
+//    public void listen(String message) {
+//        System.out.println("Received message: " + message);
+//    }
 }
--- src/main/java/ttv/poltoraha/pivka/repository/BookRepository.java
+++ src/main/java/ttv/poltoraha/pivka/repository/BookRepository.java
@@ -1,10 +1,15 @@
 package ttv.poltoraha.pivka.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ttv.poltoraha.pivka.entity.Book;
 
-@Repository
-public interface BookRepository extends CrudRepository<Book, Integer> {
+import java.util.List;
 
+@Repository
+public interface BookRepository extends JpaRepository<Book, Integer> {
+    @Query("SELECT b FROM book b WHERE b.tags LIKE %:tag%")
+    List<Book> findByTagsContaining(@Param("tag") String tag);
 }
